6d0720258842f4aff5c1036c6d6f7e7c28563f82
sebobose <sebastian.medjakovic@gmail.com>
2024-06-11 15:08:58 +0200

    Increase budget and add backend url

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_API_URL: '',
+  BASE_API_URL: 'https://mojteren-backend.onrender.com/api',
   GOOGLE_MAPS_API_KEY: 'AIzaSyDeDXOPAm_0Blq4CdoDCE6HD_MRltSWSSo',
 };
